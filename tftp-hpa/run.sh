8458172a896b4893ccde9f22b84c7914d9be6e95
Olivier Lody <olivier@nanako.debian.local>
2021-11-28 13:16:17 +0100

    m

--- tftp-hpa/run.sh
+++ tftp-hpa/run.sh
@@ -2,4 +2,5 @@
 networks=$(bashio::config 'networks')
 tftproot=$(bashio::config 'tftproot')
 echo "Starting tftpd..."
+echo "And fuck you..."
 exec /usr/sbin/in.tftpd --foreground --verbose --address :69 ${tftproot}

6deace59ef40c810ab69815e3d08dfac415354e3
Olivier Lody <olivier@nanako.debian.local>
2021-11-28 12:32:18 +0100

    test

--- tftp-hpa/run.sh
+++ tftp-hpa/run.sh
@@ -1,5 +1,14 @@
-#!/usr/bin/env bashio
-networks=$(bashio::config 'networks')
-tftproot=$(bashio::config 'tftproot')
+#!/usr/bin/env bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+tftproot=USERNAME=$(jq --raw-output '.username // empty' ${CONFIG_PATH})
+
+echo "root is : ${tftproot}"
+
+# #!/usr/bin/env bashio
+# networks=$(bashio::config 'networks')
+# tftproot=$(bashio::config 'tftproot')
 echo "Starting tftpd..."
 exec /usr/sbin/in.tftpd --foreground --verbose --address :69 ${tftproot}
